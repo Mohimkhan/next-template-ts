960ba06de25c5e8f8bc63da2204ba8833ae15fd0
Mohimkhan <mohimkhan652@gmail.com>
2025-01-31 18:32:53 +0600

    chore: add constants for logout modal ID and social media information

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const LOGOUT_MODAL_ID = "my_logout_modal";
+export const appName = "QuickType";
+export const socialMediaHandle = "Mohim Khan";
+export const socialProfileUrl = "https://www.facebook.com/mohim.khan.5076798/";
