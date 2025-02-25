0848bf782e0de99b1385c1cb9e1f6f08883925d4
Mohimkhan <mohimkhan652@gmail.com>
2025-02-25 23:36:52 +0600

    style: enhance Footer component layout and typography for better responsiveness

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -3,8 +3,8 @@ import { LinkPreview } from "../ui/link-preview";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-2 left-1/2 -translate-x-2/4 cursor-pointer dark:bg-transparent dark:text-white text-black font-semibold tracking-wider text-xs md:text-[16px]">
-      <div>
+    <div className="absolute bottom-2 left-1/2 -translate-x-2/4 w-full cursor-pointer dark:bg-transparent dark:text-white text-black font-semibold tracking-wider text-[9px] md:text-[16px]">
+      <div className="flex justify-center items-center flex-col sm:flex-row">
         <span>
           {`© ${new Date().getFullYear()} ${appName}. All rights reserved and build with ❤️ by`}
           {""}
@@ -14,7 +14,9 @@ const Footer = () => {
           isStatic={true}
           imageSrc="/images/fb_me.png"
         >
-          <span className="ml-1">{socialMediaHandle}</span>
+          <span className="ml-1 font-bold text-2xl sm:text-[9px] md:text-[16px]">
+            {socialMediaHandle}
+          </span>
         </LinkPreview>
       </div>
     </div>
