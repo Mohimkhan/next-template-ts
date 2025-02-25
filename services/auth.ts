62477155ab8bb2f49770cae581c33b7ee9362366
Mohimkhan <mohimkhan652@gmail.com>
2025-02-26 01:05:38 +0600

    feat: add GitHub provider to NextAuth authentication handlers

--- services/auth.ts
+++ services/auth.ts
@@ -1,9 +1,19 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHubProvider({
       authorization: {
         params: {
           prompt: "consent",
