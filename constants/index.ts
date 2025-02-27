04812878243d9ec629517cc317e83f533aa05467
Mohimkhan <mohimkhan652@gmail.com>
2025-02-27 15:37:05 +0600

    refactor: add type annotations to constants for improved type safety

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-export const LOGOUT_MODAL_ID = "my_logout_modal";
-export const appName = "QuickType";
-export const socialMediaHandle = "Mohim Khan";
-export const socialProfileUrl = "https://www.facebook.com/mohim.khan.5076798/";
+export const LOGOUT_MODAL_ID: string = "my_logout_modal";
+export const appName: string = "QuickType";
+export const socialMediaHandle: string = "Mohim Khan";
+export const socialProfileUrl: string = "https://www.facebook.com/mohim.khan.5076798/";
