f2a59c89f8644d59df23ddcb7129ea784ad181f9
Mohimkhan <mohimkhan652@gmail.com>
2026-01-01 20:42:49 +0600

    feat(auth.ts): enable github login and signup

--- services/auth.ts
+++ services/auth.ts
@@ -21,6 +21,11 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
+    github: {
+      prompt: "select_account",
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
   },
   trustedOrigins: [
     "http://localhost:3000",
